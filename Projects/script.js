8d8c624954dbee22a33f0865767ab84bfeff7d22
AbdelwahabKabbout <abdelwahabkabbout@gmail.com>
2025-02-05 16:57:24 +0200

    new fixing bugs , adding Projects

---
+++ Projects/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    const CurrentProject = localStorage.getItem("ProjectName");
+
+    if(CurrentProject == "Restaurant POS (C# Windows Forms)") {
+        $(".Project1").fadeIn();
+    } else if(CurrentProject == "Chess Game (Web)") {
+        $(".Project2").fadeIn();
+    } else if(CurrentProject == "Fitness Tracking App(web)") {
+        $(".Project3").fadeIn();
+    } else if(CurrentProject == "Memory Game (C# Windows Forms)") {
+        $(".Project4").fadeIn();
+    }
+
+    $(".Exit").click(function() {
+        window.location.href = "/index.html";  
+    });
+    
+});
--- script.js
+++ script.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
       $('#dropdown').stop(true, true).slideToggle(500); // Smooth dropdown effect
     });
  
-    $('#dropdown').mouseleave(function() {
+    /*$('#dropdown').mouseleave(function() {
        $('#dropdown').stop(true, true).slideToggle(500); // Smooth slide-up effect
        $('.glyphicon').toggleClass('glyphicon-thumbs-up glyphicon-thumbs-down');   
-    });
-    
+    });*/
+     $('.dropdown-content').click(function(){
+        const ProjectName = $(this).text();       
+        localStorage.setItem("ProjectName", ProjectName); 
+        window.location.href = "Projects/Project.html";
+     });
      $('#HomePage').click(function(){
          $('.HomePage').show();
          $('.AboutPage').hide();
